168dd8b858fb05c1ea08501182f63e0ca4d259eb
jefferson <salazarjefferson0210@gmail.com>
2021-12-28 12:39:56 -0500

    update config angular heroku connection

--- frontend/server.js
+++ frontend/server.js
@@ -3,14 +3,12 @@ let express = require('express'),
     port = process.env.PORT || 8080,
     path = require('path');
 
-app.use(express.static(__dirname + '/dist')); 
+app.use(express.static(__dirname + '/dist/frontend')); 
 
-app.get('/*', (req, res) => {
-    res.sendFile(path.join(__dirname + '/dist/index.html'))
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname + '/dist/frontend/index.html'))
 })
 
 app.listen(port, (err) => {
     (err) ? console.log(err):console.log(`config success on port ${port}`)
 });
-
-
