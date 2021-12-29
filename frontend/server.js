e7f08991da281b4e258119f1e0278c9fc20323ba
jefferson <salazarjefferson0210@gmail.com>
2021-12-29 11:38:47 -0500

    configuracion de angular heroku successful

--- frontend/server.js
+++ frontend/server.js
@@ -3,10 +3,11 @@ let express = require('express'),
     port = process.env.PORT || 3000,
     path = require('path');
 
-app.use(express.static(path.join(__dirname + '/src'))); 
+// app.use(express.static(path.join(__dirname + '/src/index.html'))); 
+app.use(express.static('./dist/frontend')); 
 
-app.get('/', (req, res) => {
-    res.sendFile(path.join(__dirname + '/src/index.html'))
+app.get('/*', (req, res) => {
+    res.sendFile('index.html', { root: 'dist/angular-heroku'})
 })
 
 app.listen(port, (err) => {
--- frontend/src/environments/environment.prod.ts
+++ frontend/src/environments/environment.prod.ts
@@ -1,4 +1,4 @@
 export const environment = {
   production: true,
-  baseUrl: "https://family-application.herokuapp.com/"
+  baseUrl: "https://family-application.herokuapp.com"
 };
